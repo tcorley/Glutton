efa03f0eadce4677652348f632a122a40a53462c
tcorley <tyler_corley@me.com>
2015-04-01 23:40:02 -0500

    Adding empty gitignore file and implemented basic SLPagingView to close #1 🆒

--- Glutton/AppDelegate.m
+++ Glutton/AppDelegate.m
@@ -7,8 +7,13 @@
 //
 
 #import "AppDelegate.h"
+#import "SLPagingViewController.h"
+#import "UIColor+SLAddition.h"
+
 
 @interface AppDelegate ()
+
+@property (strong, nonatomic) UINavigationController *nav;
 
 @end
 
@@ -17,7 +22,66 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    UIColor *gray = [UIColor colorWithRed: 0.5 green: 0.5 blue: 0.5 alpha: 1];
+    UIColor *orange = [UIColor colorWithRed: 0.749 green: 0.341 blue: 0 alpha: 1];
+    
+    // Make views for the navigation bar
+    UIImageView *img1 = [[UIImageView alloc] initWithImage:[[UIImage imageNamed:@"cog"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
+    
+    UIImageView *img2 = [[UIImageView alloc] initWithImage:[[UIImage imageNamed:@"user"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
+    
+    UIImageView *img3 = [[UIImageView alloc] initWithImage:[[UIImage imageNamed:@"swipe"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
+    
+    UIImageView *img4 = [[UIImageView alloc] initWithImage:[[UIImage imageNamed:@"collection"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
+    
+    SLPagingViewController *pageViewController = [[SLPagingViewController alloc] initWithNavBarItems:@[img1, img2, img3, img4] navBarBackground:[UIColor whiteColor] views:@[[self viewWithBackground:[UIColor blueColor]], [self viewWithBackground:[UIColor redColor]], [self viewWithBackground:[UIColor orangeColor]], [self viewWithBackground:[UIColor yellowColor]]] showPageControl:NO];
+    
+    pageViewController.navigationSideItemsStyle = SLNavigationSideItemsStyleOnBounds;
+    float minX = 45.0;
+    pageViewController.pagingViewMoving = ^(NSArray *subviews){
+        float mid  = [UIScreen mainScreen].bounds.size.width/2 - minX;
+        float midM = [UIScreen mainScreen].bounds.size.width - minX;
+        for(UIImageView *v in subviews){
+            UIColor *c = gray;
+            if(v.frame.origin.x > minX
+               && v.frame.origin.x < mid)
+                // Left part
+                c = [UIColor gradient:v.frame.origin.x
+                                  top:minX+1
+                               bottom:mid-1
+                                 init:orange
+                                 goal:gray];
+            else if(v.frame.origin.x > mid
+                    && v.frame.origin.x < midM)
+                // Right part
+                c = [UIColor gradient:v.frame.origin.x
+                                  top:mid+1
+                               bottom:midM-1
+                                 init:gray
+                                 goal:orange];
+            else if(v.frame.origin.x == mid)
+                c = orange;
+            v.tintColor= c;
+        }
+    };
+    
+    [pageViewController setCurrentIndex:2 animated:NO];
+    
+    self.nav = [[UINavigationController alloc] initWithRootViewController:pageViewController];
+    [self.window setRootViewController:self.nav];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    [self setWindow:self.window];
     return YES;
+}
+
+-(UIView *)viewWithBackground:(UIColor *)color {
+    UIView *view = [UIView new];
+    view.backgroundColor = color;
+    return view;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
