314f512119bd0e4c604e3be4b12a8fb3878a5b5f
Tyler Corley <tyler_corley@me.com>
2015-10-27 00:20:28 -0700

    Pod updates for iOS9 💅🏾

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment thisine to define a global platform for your project
-platform :ios, "8.2"
+platform :ios, "9.1"
 
 target "Glutton" do
 
--- Pods/Target Support Files/Pods-Glutton-MDCSwipeToChoose/Pods-Glutton-MDCSwipeToChoose-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Glutton_MDCSwipeToChoose : NSObject
-@end
-@implementation PodsDummy_Pods_Glutton_MDCSwipeToChoose
-@end
